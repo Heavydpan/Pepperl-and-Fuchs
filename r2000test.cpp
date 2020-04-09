c063c63e478ac89cd222540ea681ec5cd0d50619
Heavydpan <heavyd@qq.com>
2020-04-09 17:04:58 +0800

    Example

---
+++ r2000test.cpp
@@ -0,0 +1,13 @@
+#include "R2000DETAPI.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+int main(int argc, char **argv)
+{
+  R2000DET test;
+  bool t=test.InitAPI();
+  if(t)
+    std::cout<<"INIT API SUCESS"<<std::endl;
+  return 0;
+}
