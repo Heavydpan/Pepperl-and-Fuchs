6a564a0d89c6b886a8889bc4d6a61d840bff80a4
Pan Zhi Xiang <zxpan@AP.P-F.BIZ>
2020-04-16 11:47:04 +0800

    Updated

--- R2000DETAPI.cpp
+++ R2000DETAPI.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //#include "string.h"
 #include "R2000DETAPI.h"
 #include "ros/ros.h"
@@ -424,4 +425,433 @@ bool R2000DET::EndAPI()
 	CW_CCW = true;
 	iVersion = -1;
 	return true;
+=======
+//#include "string.h"
+#include "R2000DETAPI.h"
+#include "ros/ros.h"
+#include "std_msgs/Int8MultiArray.h"
+#include "std_msgs/Int8.h"
+//#include "math.h"
+
+char *R2000DET::ZoneBuffer = nullptr;
+bool *R2000DET::Zone_Status = nullptr;
+double R2000DET::x1 = 0;
+double R2000DET::y1 = 0;
+int R2000DET::ZoneofGroup = -1;
+int R2000DET::iVersion = -1;
+R2000DET::R2000DET()
+{
+	
+}
+
+
+R2000DET::~R2000DET()
+{
+}
+void R2000DET::R2000StatusCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+	if (ZoneBuffer == nullptr || iVersion!=1)
+		return;
+	bool *Zone_Status = isInZone_ROS(msg);
+
+	ros::NodeHandle nh;//节点Handle
+	ros::Publisher r2000detdata_pub = nh.advertise<std_msgs::Int8MultiArray>("R2000Status", 1000);
+	std_msgs::Int8MultiArray status;
+
+	status.data.resize(ZoneofGroup);
+	for (int i = 0; i<ZoneofGroup; i++)
+	{
+		if (Zone_Status[i])
+			status.data[i] = 1;
+		else
+			status.data[i] = 0;
+	}
+	r2000detdata_pub.publish(status);
+
+	delete[]Zone_Status;
+	Zone_Status = nullptr;
+}
+
+bool R2000DET::InitAPI()
+{
+	char filename[] = "Config";
+
+	FILE *f;
+	f = fopen(filename, "r");
+	if (f == NULL)
+	{
+		//		perror("open file error:");//只有上面的函数设置了error全局错误号，才可使用，会根据error输出对应的错误信息
+		return false;
+	}
+	fseek(f, 0L, SEEK_END); /* 定位到文件末尾 */
+	int64_t flen = ftell(f); /* 得到文件大小 */
+							 //ByteofConfigBuffer = flen;
+	buff = new char[flen + 1];
+	fseek(f, 0L, 0);
+	fread(buff, sizeof(char), flen, f);
+	fclose(f);
+	GetGroupNum();
+	b_Init = true;
+	return true;
+}
+
+bool R2000DET::GetGroupNum()
+{
+	if (buff == nullptr)
+		return false;
+	AllGroupNum = *(int*)buff;
+	return true;
+}
+
+bool R2000DET::StartGroup(int i_Group)
+{
+	if (iVersion == -1)
+	{
+		if (buff == nullptr)//未初始化
+			return false;
+		ZoneBuffer = buff + GetConfigFilePos(i_Group, 1, buff) - sizeof(int);//含区域数
+		ZoneofGroup = *(int*)ZoneBuffer;
+		iVersion = 0;//普通版本标志
+		return true;
+	}
+	return false;
+	//因为每次仅启动一个分组，以下代码作废
+	/*	char *tmp = nullptr;
+	char *tmp1 = nullptr;//用于复制ZoneBuffer
+	int64_t start = GetConfigFilePos(i_Group, 1, buff) - sizeof(int);//含区域数
+	int64_t end = 0;
+	if ((i_Group) <= AllGroupNum)
+	{
+	end = GetConfigFilePos(i_Group + 1, 1, buff) - sizeof(int);
+	}
+	int64_t size = end - start;//相当于tmp的长度
+	tmp = (buff + GetConfigFilePos(i_Group, 1, buff) - sizeof(int));//含区域数的buff
+	tmp1 = new char[size + ByteofZoneBuffer];
+	memcpy(tmp1, ZoneBuffer, ByteofZoneBuffer);
+	memcpy(tmp1 + ByteofZoneBuffer, tmp, size);
+	GroupNumInZoneBuffer++;
+	memcpy(tmp1, &GroupNumInZoneBuffer, sizeof(int));
+	if (ZoneBuffer != nullptr)
+	delete[]ZoneBuffer;
+	ZoneBuffer = tmp1;
+	ByteofZoneBuffer += size;*/
+
+
+}
+
+bool R2000DET::StartGroup_ROS(int i_Group, char Topic[])
+{
+	if (iVersion == -1)
+	{
+		if (buff == nullptr)//未初始化
+			return false;
+		ZoneBuffer = buff + GetConfigFilePos(i_Group, 1, buff) - sizeof(int);//含区域数
+		ZoneofGroup = *(int*)ZoneBuffer;
+		iVersion = 1;//普通版本标志
+		char **tmp2 = nullptr;
+		int tmp1 = 0;
+
+		ros::init(tmp1, tmp2, "R2000DETData");
+		ros::NodeHandle n;
+		ros::Subscriber sub = n.subscribe(Topic, 1000, R2000DET::R2000StatusCallback);
+		ros::spin();
+
+		return true;
+	}
+	return false;
+}
+
+int64_t R2000DET::GetConfigFilePos(int i_Group, int i_Zone, char *buff)
+{
+	int64_t filelen1, tmpp = 0;
+
+	int tmpq1 = 0, tmpz1 = 0;//tmpq一个用于读取当前组下区域数
+
+	char * tmp1;
+	tmp1 = buff;//指针移动，所有区域和组的数据
+	tmpz1 = (*(int*)tmp1);
+	tmp1 += sizeof(int);
+	tmpq1 = (*(int*)tmp1);
+	tmp1 += sizeof(int);
+	filelen1 = sizeof(int) * 2;//定位
+							   //bool flag = false;//用于区域数减1的标志；
+	int i = 0, j = 0;
+
+	for (i = 0; i < tmpz1; i++)
+	{
+		//找到需要删除的组和区域编号，进行定位
+		for (j = 0; j < tmpq1; j++)
+		{
+			if (i == (i_Group - 1) && j == (i_Zone - 1))//i_Zone减2是因为要停在点数的开始
+				break;
+			tmpp = *(int64_t*)tmp1;
+			filelen1 += tmpp * sizeof(CONFIG_XY) + sizeof(int64_t);
+			tmp1 += tmpp * sizeof(CONFIG_XY) + sizeof(int64_t);
+		}
+		if (i == (i_Group - 1) && j == (i_Zone - 1))//减2是因为要停在点数的开始
+		{
+			//					filelen1 += sizeof(int);
+			break;
+		}
+		tmpq1 = *(int*)tmp1;
+		tmp1 += sizeof(int);
+		filelen1 += sizeof(int);
+	}
+
+	return filelen1;
+}
+bool R2000DET::StopGroup(int i_Group)
+{
+	//	ByteofZoneBuffer = sizeof(int);
+	if (ZoneBuffer != nullptr)
+	{
+		delete[]ZoneBuffer;
+		ZoneBuffer = nullptr;
+		Zone_Status = nullptr;
+		if (iVersion == 1)
+		{
+
+		}
+		ZoneofGroup = -1;
+	}
+	iVersion = -1;
+	return true;
+}
+bool R2000DET::SetCW_CCW(bool CW_CCW)
+{
+	if (buff == nullptr)
+		return false;
+	CW_CCW = CW_CCW;
+	return true;
+}
+bool* R2000DET::isInZone_ROS(const sensor_msgs::LaserScan::ConstPtr& PointBuffer)
+{
+	if (PointBuffer == nullptr || ZoneBuffer == nullptr)
+		return nullptr;
+	char *Buffer = ZoneBuffer;
+
+
+	int *qs = (int*)Buffer;
+	Zone_Status = new bool[*qs];
+	memset(Zone_Status, 0, sizeof(bool)*(*qs));
+
+	uint64_t ds = 0;
+	Buffer += sizeof(int);
+	int i = 0, count = 0;//count为连续计数;
+	uint64_t tmpi = 0;
+	float tmpx = -1;
+	float tmpy = -1;
+
+	double angular_increment_real = PointBuffer->angle_increment;//360 / (double)R2000Head.num_points_scan;弧度
+
+	double distance;//uint32_t，按照博智林要求改
+	double ang;
+
+	for (uint32_t i1 = 0; i1 <PointBuffer->ranges.size(); i1++)
+	{
+
+		distance =(double) PointBuffer->ranges[i1]*1000;//博智林，单位米
+//		if (distance == 0xFFFFFFFF) //按照博智林要求删除
+//			break;
+
+		ang = PointBuffer->angle_min + angular_increment_real*i1;
+		y1 = distance*cos(ang) / 62.5;
+		x1 = distance*sin(ang) / 62.5;
+
+		//坐标平移，以(480,480)为圆心。注意原始config坐标数据是屏幕坐标，需要转换
+		x1 += 480;
+		y1 += 480;
+
+		for (i = 0; i < *qs; i++)
+		{
+			ds = *(uint64_t*)(Buffer); //+ds[0]*j*sizeof(CONFIG_XY));
+			CONFIG_XY* xy = nullptr;//new CONFIG_XY[ds[0]+1];
+			xy = (CONFIG_XY*)(Buffer + sizeof(int64_t));// *(j + 1) + sizeof(CONFIG_XY)*ds[0] * j);
+			for (uint64_t j = 0; j < ds; j++)
+			{
+				if (fabs((float)y1 - xy[j].y) <= 0.1)//找Y轴
+				{
+					tmpy = xy[j].y;
+					tmpx = xy[j].x;
+					if (isInOneZone(tmpx, tmpy, x1, Buffer))//相当于判断X轴
+					{
+
+						if (tmpi == 0 && i1 == 0)
+							count++;
+						else
+						{
+							if (tmpi >= 0 && (i1 - tmpi) == 1)//判断是否连续
+							{
+								count++;
+								tmpi = i1;
+							}
+							else
+							{
+								tmpi = i1;
+								count = 1;
+							}
+						}
+						if (count == 2)//连续2个点
+						{
+							Zone_Status[i] = true;
+							count = 0;
+						}
+						break;
+					}
+				}
+
+
+			}
+			Buffer += sizeof(int64_t) + ds * sizeof(CONFIG_XY);
+		}
+		Buffer = ZoneBuffer + sizeof(int);
+	}
+	return Zone_Status;
+
+}
+bool* R2000DET::isInZone(char* PointBuffer)
+{
+	if (PointBuffer == nullptr || ZoneBuffer == nullptr)
+		return nullptr;
+	char *Buffer = ZoneBuffer;
+	bool *Zone_Status = nullptr;//每个区域状态
+								//	Buffer += sizeof(int);//因为包含分组数量
+	int *qs = (int*)Buffer;
+	Zone_Status = new bool[*qs];
+	memset(Zone_Status, 0, sizeof(bool)*(*qs));
+
+	uint64_t ds = 0;
+	Buffer += sizeof(int);
+	int i = 0, count = 0;//count为连续计数;
+	uint64_t tmpi = 0;
+	float tmpx = -1;
+	float tmpy = -1;
+	R2000Head = *(tmpData*)PointBuffer;
+	double angular_increment_real = 360 / (double)R2000Head.num_points_scan;
+	PointBuffer += sizeof(R2000Head);
+	uint32_t distance;
+	double ang;
+	uint32_t times = (uint32_t)((R2000Head.packet_size - sizeof(R2000Head)) / 6);
+	for (uint32_t i1 = 0; i1 <times; i1++)
+	{
+
+		distance = *(uint32_t*)(PointBuffer + i1 * 6);
+		if (distance == 0xFFFFFFFF)
+			break;
+		if (CW_CCW)
+			ang = R2000Head.first_angle / 10000 + angular_increment_real*i1;
+		else
+			ang = R2000Head.first_angle / 10000 - angular_increment_real*i1;
+		a2b(ang, distance / 62.5);
+		//坐标平移，以(480,480)为圆心。注意原始config坐标数据是屏幕坐标，需要转换
+		x1 += 480;
+		y1 += 480;
+
+		for (i = 0; i < *qs; i++)
+		{
+			ds = *(uint64_t*)(Buffer); //+ds[0]*j*sizeof(CONFIG_XY));
+			CONFIG_XY* xy = nullptr;//new CONFIG_XY[ds[0]+1];
+			xy = (CONFIG_XY*)(Buffer + sizeof(int64_t));// *(j + 1) + sizeof(CONFIG_XY)*ds[0] * j);
+			for (uint64_t j = 0; j < ds; j++)
+			{
+				if (fabs((float)y1 - xy[j].y) <= 0.1)//找Y轴
+				{
+					tmpy = xy[j].y;
+					tmpx = xy[j].x;
+					if (isInOneZone(tmpx, tmpy, x1, Buffer))//相当于判断X轴
+					{
+
+						if (tmpi == 0 && i1 == 0)
+							count++;
+						else
+						{
+							if (tmpi >= 0 && (i1 - tmpi) == 1)//判断是否连续
+							{
+								count++;
+								tmpi = i1;
+							}
+							else
+							{
+								tmpi = i1;
+								count = 1;
+							}
+						}
+						if (count == 2)//连续2个点
+						{
+							Zone_Status[i] = true;
+							count = 0;
+						}
+						break;
+					}
+				}
+
+
+			}
+			Buffer += sizeof(int64_t) + ds * sizeof(CONFIG_XY);
+		}
+		Buffer = ZoneBuffer + sizeof(int);
+	}
+	return Zone_Status;
+
+
+}
+bool R2000DET::isInOneZone(float tmpx, float tmpy, double x, char *Buffer)
+{
+	bool flag = false;
+	uint64_t ds;
+	//ds = *(uint64_t*)(ZoneBuffer);
+	ds = *(uint64_t*)Buffer;
+	CONFIG_XY* xy = nullptr;//new CONFIG_XY[ds[0]+1];
+	xy = (CONFIG_XY*)(Buffer + sizeof(uint64_t));// *(j + 1) + sizeof(CONFIG_XY)*ds[0] * j);
+	for (uint64_t j = 0; j < ds; j++)
+	{
+		if ((tmpy >= 0) && (fabs((float)tmpy - xy[j].y) <= 0.1))
+		{
+
+			if (x <= (tmpx >= xy[j].x ? tmpx : xy[j].x) && (x >= (tmpx <= xy[j].x ? tmpx : xy[j].x)))
+			{
+				flag = true;
+				break;
+			}
+
+		}
+
+	}
+	return flag;
+}
+
+bool* R2000DET::GetStatus(char* PointBuffer)
+{
+	/*	if (Zone_Status != nullptr)
+	{
+	delete[]Zone_Status;
+	Zone_Status = nullptr;
+	}*/
+	if (iVersion == 0)
+	{
+		bool* Status = isInZone(PointBuffer);
+		return Status;
+	}
+	return nullptr;
+}
+void R2000DET::a2b(double angle, double distance)
+{
+	//	x1 = distance*cos(angle / 180 * 3.1415926535);
+	//	y1 = distance*sin(angle / 180 * 3.1415926535);
+	y1 = distance*cos(angle / 180 * 3.1415926535);
+	x1 = distance*sin(angle / 180 * 3.1415926535);
+}
+bool R2000DET::EndAPI()
+{
+	b_Init = false;
+	if (Zone_Status != nullptr)
+		delete[]Zone_Status;
+	if (ZoneBuffer != nullptr)
+		delete[]ZoneBuffer;
+	if (buff != nullptr)
+		delete[]buff;
+	CW_CCW = true;
+	iVersion = -1;
+	return true;
+>>>>>>> a5423eb... Updated
 }
--- R2000DETAPI.h
+++ R2000DETAPI.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 #include "stdio.h"
 #include "ros/ros.h"
@@ -81,4 +82,89 @@ private:
 	bool CW_CCW = true;
 	static void R2000StatusCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 
+=======
+#include <iostream>
+#include "stdio.h"
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#pragma once
+
+typedef struct
+{
+	int i_TotalGroup;
+}CONFIG_HEAD;
+typedef struct
+{
+	//		int i_Group=15;//分组标志位
+	int i_TotalZone;//对应分组的区域数
+}CONFIG_GROUP_NUM;
+typedef struct
+{
+	//		int i_Zone=16;//区域标志位
+	int64_t l_NumofPoints;//对应区域点数
+}CONFIG_ZONE_NUM;
+typedef struct//对应区域编号的坐标数据
+{
+	float x;
+	float y;
+}CONFIG_XY;
+
+class R2000DET
+{
+public:
+	R2000DET();
+	~R2000DET();
+
+	bool b_Init = false;//是否初始化成功
+	bool InitAPI();
+	bool SetCW_CCW(bool CW_CCW);//true:CCW,false:CW
+	bool StartGroup(int i_Group);
+	bool StartGroup_ROS(int i_Group, char Topic[]);
+	bool StopGroup(int i_Group);
+	bool EndAPI();
+	bool *GetStatus(char* PointBuffer);//返回避障判断信息
+	int AllGroupNum = -1;//Config的所有组数
+	static int ZoneofGroup;//当前组的区域数
+	int Group = -1;//当前使用的分组编号
+	struct tmpData
+	{
+		uint16_t magic;
+		uint16_t packet_type;
+		uint32_t packet_size;
+		uint16_t header_size;
+		uint16_t scan_number;
+		uint16_t packet_number;
+		uint64_t timestamp_raw;
+		uint64_t timestamp_sync;
+		//	UINT32 status_flags;
+		uint16_t status_flags;
+		uint16_t scan_frequency;
+		uint16_t status_flags1;
+		uint16_t num_points_scan;
+		uint16_t num_points_packet;
+		uint16_t first_index;
+		int32_t first_angle;
+		int32_t angular_increment;
+		uint32_t output_status;
+		uint32_t field_status;
+	}R2000Head;
+private:
+	bool GetGroupNum();
+	int64_t GetConfigFilePos(int i_Group, int i_Zone, char *buff);
+	//int64_t ByteofConfigBuffer = 0;
+	//int GroupNumInZoneBuffer = 0;//ZoneBuffer里面包含的分组数
+	bool* isInZone(char* PointBuffer);//PointBuffer R2000原始数据
+	static		bool* isInZone_ROS(const sensor_msgs::LaserScan::ConstPtr& PointBuffer);
+	static bool isInOneZone(float tmpx, float tmpy, double x, char *Buffer);
+	static int iVersion;//0:普通版本，1:ROS，2、3……，默认：-1，未选择
+	void a2b(double angle, double distance);
+	static double x1;
+	static double y1;
+	char * buff = nullptr;//所有Config
+	static	char * ZoneBuffer;//每次仅启用一个分组，所以是区域数+区域数据
+	static bool *Zone_Status;//每个区域状态
+	bool CW_CCW = true;
+	static void R2000StatusCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+
+>>>>>>> a5423eb... Updated
 };
